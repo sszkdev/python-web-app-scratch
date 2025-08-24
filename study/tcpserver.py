427ad9bdf1d8a2ddc52759fc90ea6064618cd3a4
suzuking19 <suzuki.coding.19@gmail.com>
2025-08-24 16:09:07 +0900

    homebrew server

---
+++ study/tcpserver.py
@@ -0,0 +1,37 @@
+import socket
+
+class TCPServer():
+    def serve(self):
+        print("=== start a server ===")
+
+        try:
+            # create socket
+            server_socket = socket.socket()
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+            # assign the socket to localhost:8080
+            server_socket.bind(("localhost", 8080))
+            server_socket.listen(10)
+
+            # wait an external connection and establish when one is received
+            print("=== wait a connection from client ===")
+            (client_socket, address) = server_socket.accept()
+            print(f"=== client connection complete, remote_address: {address} ===")
+
+            # retrieve the data sent from client
+            request = client_socket.recv(4096)
+
+            # write data sent from client to a file
+            with open("server_recv.txt", "wb") as f:
+                f.write(request)
+
+            # end the connection without response 
+
+        finally:
+            print("=== shut down the server ===")
+
+
+
+if __name__ == "__main__":
+    server = TCPServer()
+    server.serve()
